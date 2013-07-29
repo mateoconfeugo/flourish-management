aa40de868f040600af9d25a7c43baa9f317c632a
Matt Burns <matthewburns@gmail.com>
2013-07-29 10:54:32 -0700

    bz-28 created wrapper attempting to deploy with heroku

--- src/management/controllers/admin.clj
+++
@@ -1,9 +0,0 @@
-(ns management.controllers.admin
-  (:use [compojure.core :only [defroutes GET]]
-        [management.views.admin :as admin :only [admin-dashboard]]
-        [ring.util.response :only [content-type file-response]]))
-
-(defroutes admin-mgmt-routes
-  (GET "/clientconfig" [] (content-type (file-response "clientconfig.json" {:root "resources"})  "application/json"))
-  (GET "/admin/:admin-id" [id] (admin-dashboard {})))
-
